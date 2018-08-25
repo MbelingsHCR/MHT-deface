e3b21bf1cb96caa1dfacab9e15adf2c0e22aab37
21D4N404 <42216966+21D4N404@users.noreply.github.com>
2018-08-25 16:22:50 +0700

    Add files via upload

---
+++ Files/var.php
@@ -0,0 +1,37 @@
+<?php
+$rhversion = "2.0.0";
+$white = "\e[97m";
+$black = "\e[30m\e[1m";
+$yellow = "\e[93m";
+$orange = "\e[38;5;208m";
+$blue   = "\e[34m";
+$lblue  = "\e[36m";
+$cln    = "\e[0m";
+$green  = "\e[92m";
+$fgreen = "\e[32m";
+$red    = "\e[91m";
+$magenta = "\e[35m";
+$bluebg = "\e[44m";
+$lbluebg = "\e[106m";
+$greenbg = "\e[42m";
+$lgreenbg = "\e[102m";
+$yellowbg = "\e[43m";
+$lyellowbg = "\e[103m";
+$redbg = "\e[101m";
+$grey = "\e[37m";
+$cyan = "\e[36m";
+$bold   = "\e[1m";
+function redhawk_banner(){
+  echo "\e[91;1m
+                \e[36m             Tool\e[91m 
+                     Indonesian C\e[97myber Army
+\e[34m
+   _ _                       _                 _ _ _   __  _ _
+ / __| |_  ___ _ __  ___  __| |      ____ _ __| | | | /  \| | |
+| (__| ' \/ -_) '  \/ _ \/ _` |     |_ / '_/ _` |_  _| () |_  _|
+ \___|_||_\___|_|_|_\___/\__,_|__ __/__|_| \__,_| |_| \__/  |_|
+                             |___|___|
+     \e[91m         php {C} Coded By - Chemod_zrd404                                                            \e[32m
+  \n";
+}
+?>
