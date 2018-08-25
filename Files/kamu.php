d1424a8611b88b1204ed655b4fa9558210859724
21D4N404 <42216966+21D4N404@users.noreply.github.com>
2018-08-25 17:13:43 +0700

    Update kamu.php

--- Files/kamu.php
+++ Files/kamu.php
@@ -149,10 +149,6 @@ askscan:
           {
             goto thephuckinstart;
           }
-      
-      if [ $back = "B" ] || [ $back = "b" ] then
-        # sh ICA.sh
-        if
         elseif ($scan == 'q' | $scan == 'Q')
           {
             echo "\n\n\t Good Bye - Have a nice day :)\n\n";
