953e35dcf5abdb4daa23b3f0b9f1a1e9e9817c06
21D4N404 <42216966+21D4N404@users.noreply.github.com>
2018-08-25 13:15:08 +0700

    Update ICA.sh

--- ICA.sh
+++ ICA.sh
@@ -17,15 +17,16 @@ echo $cyan" ["$red"+"$cyan"]Blog        : http://generasicyberindo.blogspot.com"
 echo $cyan" ["$red"+"$cyan"]Github	: https://github.com/21D4N404"
 echo $green" ["$white"NOTE : simpan script di memory internal di luar folder"$green"]"
 echo $red"##############################################################"
-echo $indigo"	~"$cyan"{"$white"A"$cyan"}"$green".Tebas Index"$indigo"+"$cyan"       {"$white"Q"$cyan"}"$green"Quit "
-echo $indigo"	~"$cyan"{"$white"B"$cyan"}"$green".Colong Script$indigo"+""$cyan"     {"$white"I"$cyan"}"$green"Info "
+echo $indigo"	~"$cyan"{"$white"A"$cyan"}"$green".Tebas Index"$indigo      "+"$cyan"{"$white"Q"$cyan"}"$green"Quit "
+echo $indigo"	~"$cyan"{"$white"B"$cyan"}"$green".Colong Script$indigo     "+""$cyan"{"$white"I"$cyan"}"$green"Info "
 echo $indigo"	~"$cyan"{"$white"C"$cyan"}"$green".Auto Deface"$cyan
 echo $red"＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿"
 echo $indigo"	~"$cyan"{"$white"T"$cyan"}"$white".TUTORIAL & LIVE TARGET"$cyan
 echo $red"＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿"
-echo $green"     Pilih Mau Yg Mana?"
+echo 
 echo $white "╭─"$yellow"Chemod@zrd404"$cyan" ~/IndoCyber"$white
 read -p " ╰─$ "  apaan
+echo $indigo"☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆"
 
 if [ $apaan = "A" ] || [ $apaan = "a" ]
 then
