7a94a42f4bf45b17ea6acc795a46bc8f714e0f51
21D4N404 <42216966+21D4N404@users.noreply.github.com>
2018-08-30 17:35:04 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,49 @@
+clear
+blue='\e[1;34m'
+green='\e[1;32m'
+purple='\e[1;35m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+white='\e[1;37m'
+yellow='\e[1;33m'
+echo -e $green
+echo "risky channel./";
+sleep 0.1;
+clear
+echo -e $green
+echo '21D4N404..-';
+sleep 0.1;
+clear
+echo -e $red
+echo 'Chemod_zrd404...\';
+sleep 0.1;
+clear
+echo -e $green
+echo "21D4N404..../";
+sleep 0.1;
+clear
+echo -e $red
+echo 'Chemod_zrd404.....-';
+sleep 0.1;
+clear
+echo -e $green
+echo '21D4N404......\';
+sleep 0.1;
+clear
+echo -e $red
+echo "Chemod_zrd404......./";
+sleep 0.1;
+clear
+echo -e $green
+echo "21D4N404........-";
+sleep 0.1;
+clear
 apt upgrade && apt update
 apt install python2
 apt install figlet
 apt install curl
 apt install bash
+clear
+echo -e $green      "Install berhasil..."
+echo ""
+echo -e $yellow "ktikan : sh ICA.sh"
