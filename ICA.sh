d4ce5617605615e1bc6c9d9115e1487b0e291b5e
21D4N404 <42216966+21D4N404@users.noreply.github.com>
2018-08-31 10:03:04 +0700

    Update ICA.sh

--- ICA.sh
+++ ICA.sh
@@ -1,3 +1,54 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+import platform
+import time
+
+g = "\033[32;1m"
+gt = "\033[0;32m"
+bt = "\033[34;1m"
+b = "\033[36;1m"
+m = "\033[31;1m"
+c = "\033[0m"
+p = "\033[37;1m"
+u = "\033[35;1m"
+M = "\033[3;1m"
+k = "\033[33;1m"
+kt = "\033[0;33m"
+a = "\033[30;1m"
+
+W = '\x1b[0m'
+R = '\x1b[31m'
+G = '\x1b[1;32m'
+O = '\x1b[33m'
+B = '\x1b[34m'
+P = '\x1b[35m'
+C = '\x1b[36m'
+GR = '\x1b[37m'
+
+
+
+def slowprints(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(2.0/90)
+def lodprint(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(7.0/90)
+
+semut=(gt+"""
+#     # ####### #        #####  ####### #     # #######
+#  #  # #       #       #     # #     # ##   ## #
+#  #  # #       #       #       #     # # # # # #
+#  #  # #####   #       #       #     # #  #  # #####
+#  #  # #       #       #       #     # #     # #
+#  #  # #       #       #     # #     # #     # #
+ ## ##  ####### #######  #####  ####### #     # #######
+ ======================================================
+""")
 green="\033[32;1m"
 yellow="\033[33;1m"
 indigo="\033[34;1m"
@@ -5,7 +56,6 @@ red="\033[35;1m"
 purple="\033[37;1m"
 cyan="\033[36;1m"
 white="\033[39;1m"
-bash install.sh
 echo
 echo  $green
 echo "Loading.......";
